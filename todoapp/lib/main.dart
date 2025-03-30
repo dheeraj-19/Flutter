8d9ce9372106820e323ae917c286baa5eb641fee
Dheeraj Manchandia <dheerajmanchandia@gmail.com>
2025-03-30 19:21:09 -0400

    Updating the ToDoApp

--- todoapp/lib/main.dart
+++ todoapp/lib/main.dart
@@ -11,10 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'To Do App',
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
